c0410763d8f96ad43bf798ee2539bfdaa8dd94f3
Lilia Sviridova <svirivoda@gmail.com>
2022-09-27 10:14:49 +0300

    Create the project file structure

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const userRouter = require("./routes/users.js");
+const cardRouter = require("./routes/cards.js");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "63307f3ec55b1b5e51d826e5",
+  };
+
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/users", userRouter);
+app.use("/cards", cardRouter);
+
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+const User = require("../models/user");
+
+module.exports.getUser = (req, res) => {
+  User.find({})
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+module.exports.getUserId = (req, res) => {
+  User.find({})
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+module.exports.updateUser = (req, res) => {
+  const query = { _id: req.user._id };
+  User.findOneAndUpdate(query, req.body, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const query = { _id: req.user._id };
+  User.findOneAndUpdate(query, req.body, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+};
